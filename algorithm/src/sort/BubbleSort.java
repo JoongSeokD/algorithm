186d291e8815dc75c80d2225e5872a160805a51c
JoongSeokD <ljseokd@gmail.com>
2020-01-02 16:56:27 +0900

    버블정렬 버전2 (패스가 이루어지지 않을때 break로 알고리즘 개선)

--- algorithm/src/sort/BubbleSort.java
+++ algorithm/src/sort/BubbleSort.java
@@ -13,10 +13,16 @@ public class BubbleSort {
 	
 	// 버블 정렬
 	static void bubbleSort(int[] a, int n) {
-		for (int i = 0; i < n - 1; i++) 
-			for (int j = n - 1; j > i; j--) 
-				if (a[j - 1] > a[j]) 
+		for (int i = 0; i < n - 1; i++) {
+			int exchg = 0; //패스의 교환 횟수를 기록
+			for (int j = n - 1; j > i; j--) {
+				if (a[j - 1] > a[j]) {
 					swap(a, j - 1, j);
+					exchg++;
+				}
+			}
+			if(exchg == 0) break; // 교환이 이루어지지 않으면 종료
+		}
 	}
 	
 	public static void main(String[] args) {
