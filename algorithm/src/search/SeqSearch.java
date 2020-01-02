d324ff3b4806142ea0929f35d7f154816ea741bd
JoongSeokD <ljseokd@gmail.com>
2020-01-02 10:50:53 +0900

    for문을 활용한 선형검색

--- algorithm/src/search/SeqSearch.java
+++ algorithm/src/search/SeqSearch.java
@@ -6,15 +6,12 @@ public class SeqSearch {
 	
 	// 요소의 수가 n인 배열 a에서 key와 같은 요소를 선형 검색합니다. 
 	static int seqSeach(int[] a, int n, int key) {
-		int i = 0;
 		
-		while (true) {
-			if (i == n) 
-				return -1; // 검색실패 (-1을 반환)
-			if (a[i] == key)  
-				return i; // 검색 성공(인덱스를 반환)
-			i++;
-		}
+		for (int i = 0; i < a.length; i++) 
+			if(a[i] == key)
+				return i;
+		
+		return -1;
 	}
 	
 	public static void main(String[] args) {
