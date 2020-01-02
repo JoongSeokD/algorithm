f102ecf69715d583c48075a983cfe5e23d88376b
JoongSeokD <ljseokd@gmail.com>
2020-01-02 11:36:12 +0900

    while문 선형검색에 보초법을 추가하여 반복문에서 종료판단 횟수를 2회에서 1회로 줄임

---
+++ algorithm/src/search/SeqSearchSen.java
@@ -0,0 +1,40 @@
+package search;
+
+import java.util.Scanner;
+
+public class SeqSearchSen {
+	
+	// 요소의 수가 n인 배열 a에서 key와 같은 요소를 선형 검색합니다. 
+	static int seqSeach(int[] a, int n, int key) {
+		
+		for (int i = 0; i < a.length; i++) 
+			if(a[i] == key)
+				return i;
+		
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("요소의 수 : ");
+		int num = sc.nextInt();
+		int[] x = new int[num];
+		
+		for (int i = 0; i < num; i++) {
+			System.out.print("x[" + i + "] : ");
+			x[i] = sc.nextInt();
+		}
+		
+		System.out.print("검색할 값 : ");
+		int key = sc.nextInt();
+		int idx = seqSeach(x, num, key);
+		
+		if (idx == -1) {
+			System.out.println("그 값의 요소가 없습니다.");
+		} else {
+			System.out.println(key + "은(는) x[" + idx + "]에 있습니다.");
+		}
+		
+	}
+}
